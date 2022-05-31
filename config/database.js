61cc2071cc6794c4f6e6e4a52b1b6124864f4716
Leo Constant <101950707+leonardconstant1995@users.noreply.github.com>
2022-05-31 02:33:53 -0400

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://leoconstant:leo@cluster0.ari5v8s.mongodb.net/notepad?retryWrites=true&w=majority', 
+    'dbName': 'notepad'
+};
